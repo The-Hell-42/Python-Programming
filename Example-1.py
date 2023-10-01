a00c85042be35a99968d1ddce09d869df6fcdd0a
The-Hell-42 <sahsiddhartha19@gmail.com>
2023-10-01 21:15:06 +0545

    Completed only exercise 1 and remainin will be completed by next day

--- Example-1.py
+++ Example-1.py
@@ -7,9 +7,35 @@
 print("Hello World")
 print(7*15)
 
-a = 12345
-print(a)
+a1 = 12345
+print(a1)
 harry = 4269
 print(harry)
-b = "Anurag"
-print(b)
+b2 = "Anurag"
+print(b2)
+
+# Exercise-1
+# Create a calculator
+
+
+a = int(input("Enter the First Number :"))
+o = input("Enther the operator :")
+b = int(input("Enter the Second Number :"))
+if o == "+":
+  ans1 = a + b
+  print("Addition of",a,"and",b,"is:", ans1)
+elif o == "-":
+  ans2 = a - b
+  print("Subtraction of",a,"and",b,"is:", ans2)
+elif o == "*":
+  ans3 = a * b
+  print("Multiplication of",a,"and",b,"is:", ans3)
+elif o == "/":
+  if b != 0:
+        ans4 = a / b
+        print("Division of", a, "by", b, "is:", ans4)
+  else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operator. Please enter +, -, *, or /.")
+print("Thanks for using the calculator!")
