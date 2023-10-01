93f2669336161eb6d5d4917c2122727cbfa8bb85
The-Hell-42 <sahsiddhartha19@gmail.com>
2023-09-30 21:22:54 -0700

    Today i completed basic of python tutorial till video 15

--- Example-1.py
+++ Example-1.py
@@ -1,7 +1,15 @@
 # Day1
 # Date: 9/28/2023
 # course : 100Days of Python challenge
-# completion : Day 5
+# completion : Day 15
+# There is need of practise which I will do on Date:10/01/2023
 
 print("Hello World")
 print(7*15)
+
+a = 12345
+print(a)
+harry = 4269
+print(harry)
+b = "Anurag"
+print(b)
