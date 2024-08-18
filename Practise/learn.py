909705ea37ef1e12b7d2b58a7f9978c0661f197a
The-Hell-42 <sahsiddhartha19@gmail.com>
2024-08-18 11:40:50 +0545

    All good

---
+++ Practise/learn.py
@@ -0,0 +1,28 @@
+# # print("Hello you are again back")
+# # for i in range(10):
+# #     print("Hello you", sep=" * ", end="\n")
+# # a = "Anurag"
+# # for i in a:
+# #     print(i)
+# # print(a[:6])
+# # print(a[5:])
+# # print(a[2:5])
+# # print(a[-6:])
+# # print(len(a))
+# # print(a.upper())
+# # print(a.lower())
+# # print(a.count("a"))
+# # print(a.find("a"))
+# # print(a.find(""))
+# # str1 = "Hello World"
+# # print(str1.center(50,"*"))
+# # print(str1.find("p"))
+# # print(str1.index("o"))
+# f_name = input("Enter your first name : ")
+# l_name = input("Enter your surname : ")
+# print(f"You are {f_name } {l_name} !, Welcome to Pyhton programming")
+f_name = input("Enter your name: ")
+l_name = input("Enter your surname")
+if(l_name == "Sah"):
+    l_name = "Prasad Sah"
+print(f"My name is {f_name} {l_name}, I am learning Python")
